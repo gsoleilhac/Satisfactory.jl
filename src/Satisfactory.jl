b8094f4c64ecd94c0bd11400080b6fb314af51f7
Gauthier Soleilhac <gsoleilhac@gmail.com>
2021-03-19 13:43:34 +0100

    use data.json to parse recipes

--- src/Satisfactory.jl
+++ src/Satisfactory.jl
@@ -1,6 +1,6 @@
 module Satisfactory
 
-using JuMP, Cbc, DataFrames, CSV, LightGraphs, LayeredLayouts, Plots
+using JuMP, Cbc, DataFrames, JSON3, LightGraphs, LayeredLayouts, Plots
 using InteractiveUtils: subtypes
 
 include("products.jl") ; export Product #, subtypes(Product)
@@ -11,13 +11,19 @@ include("utils.jl")
 export baseRecipes, baseResources, maxResources
 export maximize!, maximizeDiscrete!
 
-const baseRecipes = filter(x -> !occursin("Alternate", x.name), allRecipes)
+const allRecipes = Recipe[]
+const baseRecipes = Recipe[]
+const harvestedProducts = (AlienCarapace, AlienOrgans, FICSITCoupon, FlowerPetals, GreenPowerSlug, Leaves, 
+    Mycelia, PurplePowerSlug, SAMOre, Wood, YellowPowerSlug)
+const dictProductRecipes = Dict(p => Set{Tuple{Recipe, Float64}}() for p in subtypes(Product))
+const dictProductDependantRecipes = Dict(p => Set{Tuple{Recipe, Float64}}() for p in subtypes(Product))
 const baseResources = (Limestone, IronOre, CopperOre, CateriumOre, Coal, RawQuartz, Sulfur, Bauxite, Uranium, Water, CrudeOil)
 const maxResources = Dict(zip(baseResources, (52860, 70380, 28860, 11040, 31680, 10500, 3300, 7800, 1200, 100_000, 7500)))
 
+readData()
+
 function maximize!(::Type{T} ; resources::Dict, alternates = String[], 
     allowMultiRecipes = true, nodeLabelSize = 4, edgeLabelSize = 4) where T <: Product
-    
     m, x, y = maxModelLinear(T ; resources, alternates, allowMultiRecipes)
     JuMP.set_silent(m)
     optimize!(m);
--- src/recipes.jl
+++ src/recipes.jl
@@ -1,4 +1,4 @@
-@enum Building Smelter Constructor Assembler Manufacturer Refinery Miner Foundry WaterExtractor OilExtractor
+@enum Building Smelter Constructor Assembler Manufacturer Refinery Miner Foundry WaterExtractor OilExtractor Packager
 
 struct Recipe
     name::String
@@ -7,6 +7,78 @@ struct Recipe
     building::Building
     duration::Float64
 end
+
+dataPath = joinpath(@__DIR__, "data.json")
+
+function readData()
+    data = JSON3.read(read(dataPath))
+
+    _buildings = [data.buildings[b] for b in first.(filter(!isempty, unique(r.producedIn for r in values(data.recipes))))]
+    _recipes = filter(x -> !isempty(x.producedIn), collect(values(data.recipes)))
+    _products = unique(union((p.item for r in _recipes for p in r.products), (p.item for r in _recipes for p in r.ingredients)))
+    
+    classNameToProduct = Dict{String, Type{<:Product}}(data.items[p].className => nameToType(data.items[p].name) for p in _products)
+    classNameToBuilding = Dict{String, Building}(b.className => nameToType(b.name) for b in _buildings)
+
+    empty!(allRecipes)
+    empty!(dictProductDependantRecipes)
+    empty!(dictProductRecipes)
+    empty!(baseRecipes)
+
+    for r in _recipes
+        out = [(classNameToProduct[p.item], p.amount * 60 / r.time) for p in r.products]
+        in = [(classNameToProduct[p.item], p.amount * 60 / r.time) for p in r.ingredients]
+        building = classNameToBuilding[only(r.producedIn)]
+        push!(allRecipes, Recipe(r.name, out, in, building, r.time))
+    end
+
+    minerMK1 = data.miners.Build_MinerMk1_C
+    for p in minerMK1.allowedResources
+        product = classNameToProduct[p]
+        qty = minerMK1.itemsPerCycle / minerMK1.extractCycleTime * 60
+        push!(allRecipes, Recipe("$product", [(product, qty)], [], Miner, 60))
+    end
+    oilPump = data.miners.Build_OilPump_C
+    for p in oilPump.allowedResources
+        product = classNameToProduct[p]
+        qty = oilPump.itemsPerCycle / oilPump.extractCycleTime * 60 / 1000
+        push!(allRecipes, Recipe("$product", [(product, qty)], [], OilExtractor, 60))
+    end
+    waterPump = data.miners.Build_WaterPump_C
+    for p in waterPump.allowedResources
+        product = classNameToProduct[p]
+        qty = waterPump.itemsPerCycle / waterPump.extractCycleTime * 60 / 1000
+        push!(allRecipes, Recipe("$product", [(product, qty)], [], WaterExtractor, 60))
+    end
+
+    for r in allRecipes
+        for (p, qty) in r.out
+            push!(get!(dictProductRecipes, p, Set()), (r, qty))
+        end
+    end  
+    for r in allRecipes
+        for (p, qty) in r.in
+            push!(get!(dictProductDependantRecipes, p, Set()), (r, qty))
+        end
+    end
+
+    for p in subtypes(Product)
+        if p in harvestedProducts
+            dictProductRecipes[p] = Set()
+        end
+        if !haskey(dictProductDependantRecipes, p)
+            dictProductDependantRecipes[p] = Set()
+        end
+        if !haskey(dictProductRecipes, p)
+            @warn "No recipe found to make $p"
+        end
+    end
+
+    append!(baseRecipes, filter(x -> !occursin("Alternate", x.name), allRecipes))
+    nothing
+end
+
+nameToType(s::AbstractString) = getfield(Satisfactory, Symbol(replace(s, r" |-|\." => ""))) # remove spaces, dashes and dots 
 
 recipes(::Type{T}) where T <: Product = dictProductRecipes[T]
 recipes(::T) where T <: Product = recipes(T)
@@ -18,74 +90,3 @@ import Base.show, Base.==, Base.hash
 show(io::IO, r::Recipe) = print(io, "$(r.name)")
 hash(r::Recipe, h::UInt) = hash(r.name, h)
 ==(r1::Recipe, r2::Recipe) = r1.name == r2.name
-
-# https://docs.google.com/spreadsheets/d/1nMw0y-i5aoAmsw2SLmIL-88ClzCc6nt2UqiLSGr2o6Q/edit#gid=0
-# replace Filter with Gas Filter
-# replace Cartridge with Rifle Cartridge
-# replace Miner with Oil Extractor for Crude Oil
-# divide all liquid quantities by 1000
-# Fix quantities used for Plastic and Rubber
-# Fix products used for Alternate: Diluted Packaged Fuel
-const allRecipes = Recipe[]
-for row in eachrow(DataFrame(CSV.File(joinpath(@__DIR__, "recipes.txt"))))
-    name, inputs, outputs, duration, buildings, _ = row
-
-    (buildings == "Build Tool" || buildings == "Workshop") && continue
-
-    in = Tuple{DataType, Float64}[]
-    for x in split(inputs, ",")
-        item, s_qty = match(r"(.*)\((\d*)\)", x).captures
-        item = replace(item, r"\s|-|\." => "") # removes whitespaces, dots and dashes
-        qty = parse(Float64, s_qty)
-        type = eval(Symbol(item))
-        push!(in, (type, qty / duration * 60))
-    end
-
-    out = Tuple{DataType, Float64}[]
-    for x in split(outputs, ",")
-        item, s_qty = match(r"(.*)\((\d*)\)", x).captures
-        item = replace(item, r"\s|-|\." => "") # removes whitespaces, dots and dashes
-        qty = parse(Float64, s_qty)
-        type = eval(Symbol(item))
-        push!(out, (type, qty / duration * 60))
-    end
-
-    building = if occursin("Miner", buildings)
-        Miner
-    elseif occursin("Smelter", buildings)
-        Smelter
-    elseif occursin("Refinery", buildings)
-        Refinery
-    elseif occursin("Constructor", buildings)
-        Constructor
-    elseif occursin("Assembler", buildings)
-        Assembler
-    elseif occursin("Manufacturer", buildings)
-        Manufacturer
-    elseif occursin("Foundry", buildings)
-        Foundry
-    elseif occursin("Oil Extractor", buildings)
-        OilExtractor
-    end
-
-    if building == Miner || building == OilExtractor
-        empty!(in)
-        out = [(p, 2 * qty) for (p,qty) in out]
-    end
-
-    push!(allRecipes, Recipe(name, out, in, building, duration))
-end
-push!(allRecipes, Recipe("Water", [(Water, 120)], [], WaterExtractor, 6))
-
-const dictProductRecipes = Dict(p => Set{Tuple{Recipe, Float64}}() for p in subtypes(Product))
-for r in allRecipes
-    for (p, qty) in r.out
-        push!(dictProductRecipes[p], (r, qty))
-    end
-end  
-const dictProductDependantRecipes = Dict(p => Set{Tuple{Recipe, Float64}}() for p in subtypes(Product))
-for r in allRecipes
-    for (p, qty) in r.in
-        push!(dictProductDependantRecipes[p], (r, qty))
-    end
-end
